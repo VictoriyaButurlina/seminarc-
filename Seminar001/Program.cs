06c23b1da0c906b63f6554c39836e398940ce392
Victoriya <victory_alex@mail.ru>
2022-08-06 00:03:43 +0300

    Сравнение двух чисел, нахождение максимального

---
+++ Seminar001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.Write("Введите первое число: ");
+int number1= int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int max = number1;
+if (number2 > max) max = number2;
+Console.Write($"Максимальное число: {max}");
