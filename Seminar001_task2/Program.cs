410950d4f5caee75ad5fca339b97850cee593494
Victoriya <victory_alex@mail.ru>
2022-08-06 00:36:55 +0300

    Максимальное число из трех чисел

---
+++ Seminar001_task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.Write("Напишите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Напишите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.Write("Напишите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+Console.Write($"Максимальное число: {max}");
